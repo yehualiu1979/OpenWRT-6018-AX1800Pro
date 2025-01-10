dd97f897fdd2302dac25c03e91f1035d65c383a2
yehualiu1979 <braddockkins@gmail.com>
2025-01-11 00:56:24 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd ./wrt/
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
@@ -8,7 +8,7 @@ rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 ./scripts/feeds install -a 
-
+cd ./wrt/package/
 #安装和更新软件包
 UPDATE_PACKAGE() {
 	local PKG_NAME=$1
